002dd6dfdb65dbbadcd3bbf85284e769836ea0d4
aabidihassan <aabidihassan2@gmail.com>
2022-05-12 14:43:17 +0100

    first commit

---
+++ src/main/java/com/labo/budgets/LaboratoryBudgetsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.labo.budgets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryBudgetsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboratoryBudgetsManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/labo/budgets/LaboratoryBudgetsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labo.budgets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryBudgetsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
